004a115ee56236a282637f5be0b5dbb95373d934
FoOtBaLL3R <pavlo.23.94@gmail.com>
2024-06-04 05:46:40 -0600

    added a component that allows you to create a note

--- my-first-react-project/src/App.jsx
+++ my-first-react-project/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
+import { Button, Input, Textarea } from '@chakra-ui/react'
 import './App.css'
+import CreateNoteForm from './components/CreateNoteForm'
 
 function App() {
 
   return (
-    <>
-      <p style={{textAlign: "center"}}>Notes</p>
-      <h1 >My</h1>
-    </>
+    <section className='p-8 flex flex-row justify-start items-start gap-12'>
+      <div className='flex flex-col w-1/3 gap-10'>
+        <CreateNoteForm />
+      </div>
+    </section>
   )
 }
 
