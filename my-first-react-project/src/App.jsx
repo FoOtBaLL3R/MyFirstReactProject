a2dc92fe233385c4323a74d7d5c197875a52d393
FoOtBaLL3R <pavlo.23.94@gmail.com>
2024-06-05 06:07:31 -0600

    Added Filters and Notes components

--- my-first-react-project/src/App.jsx
+++ my-first-react-project/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import { Button, Input, Textarea } from '@chakra-ui/react'
+import { Select, Input } from '@chakra-ui/react'
 import './App.css'
 import CreateNoteForm from './components/CreateNoteForm'
+import Note from './components/Note'
+import Filters from './components/Filters'
 
 function App() {
 
@@ -9,7 +10,14 @@ function App() {
     <section className='p-8 flex flex-row justify-start items-start gap-12'>
       <div className='flex flex-col w-1/3 gap-10'>
         <CreateNoteForm />
+        <Filters />
+        
       </div>
+      <ul className='flex flex-col gap-5 w-1/2'>
+        <li>
+          <Note />
+        </li>
+      </ul>
     </section>
   )
 }
