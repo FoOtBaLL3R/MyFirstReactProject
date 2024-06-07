7c8c0d23260604bb0763d89d6a7318cfbfd54921
FoOtBaLL3R <pavlo.23.94@gmail.com>
2024-06-07 05:12:12 -0600

    made a request to the backend and processed it

--- my-first-react-project/src/App.jsx
+++ my-first-react-project/src/App.jsx
@@ -1,10 +1,23 @@
 import { Select, Input } from '@chakra-ui/react'
-import './App.css'
 import CreateNoteForm from './components/CreateNoteForm'
 import Note from './components/Note'
 import Filters from './components/Filters'
+import { useEffect, useState } from 'react'
+import { fetchNotes } from './services/note'
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let notesAll = await fetchNotes();
+      // console.log(notesAll);
+      setNotes(notesAll);
+    }
+
+    fetchData();
+
+  }, [])
 
   return (
     <section className='p-8 flex flex-row justify-start items-start gap-12'>
@@ -14,12 +27,15 @@ function App() {
         
       </div>
       <ul className='flex flex-col gap-5 w-1/2'>
-        <li>
-          <Note />
-        </li>
+        {notes.map((n) => (
+          <li key={n.id}>
+            <Note title={n.name} description={n.description} createdAt={n.createAt}/>
+          </li>
+        ))}
       </ul>
     </section>
   )
 }
 
 export default App
+
