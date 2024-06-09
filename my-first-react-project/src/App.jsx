39fb49d079616a61a632640d19b5695041bb8974
FoOtBaLL3R <pavlo.23.94@gmail.com>
2024-06-09 16:03:27 -0600

    implemented a search component, filtering component and component  creating notes

--- my-first-react-project/src/App.jsx
+++ my-first-react-project/src/App.jsx
@@ -3,27 +3,37 @@ import CreateNoteForm from './components/CreateNoteForm'
 import Note from './components/Note'
 import Filters from './components/Filters'
 import { useEffect, useState } from 'react'
-import { fetchNotes } from './services/note'
+import { createNote, fetchNotes } from './services/note'
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [filter, setFilter] = useState({
+    search: "",
+    sortItem: "date",
+    sortOrder: "desc",
+  });
 
   useEffect(() => {
     const fetchData = async () => {
-      let notesAll = await fetchNotes();
+      let notesAll = await fetchNotes(filter);
       // console.log(notesAll);
       setNotes(notesAll);
     }
 
     fetchData();
 
-  }, [])
+  }, [filter]);
+  const onCreate = async (note) => {
+    await createNote(note);
+    let notesAll = await fetchNotes(filter);
+    setNotes(notesAll);
+  }
 
   return (
     <section className='p-8 flex flex-row justify-start items-start gap-12'>
       <div className='flex flex-col w-1/3 gap-10'>
-        <CreateNoteForm />
-        <Filters />
+        <CreateNoteForm onCreate={onCreate}/>
+        <Filters filter={filter} setFilter={setFilter}/>
         
       </div>
       <ul className='flex flex-col gap-5 w-1/2'>
